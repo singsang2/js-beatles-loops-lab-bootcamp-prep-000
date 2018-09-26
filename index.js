d38c2e91a89b1f6093b5436439e1b27ce76db0e1
Sung Bae <singsang2@gmail.com>
2018-09-26 03:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 // add solution here
 function theBeatlesPlay(musicians, inst){
-  array = new Array();
+  var array = new Array([]);
+  for(i = 0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${inst[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(array){
+  var i = 0;
   
+  while (i < array.length) {
+    array[i] = array[i]+"!!!";
   
+  return array;
+  }
+}
+
+function iLoveTheBeatles(number){
+  var array = new Array([]);
+  
+  do {
+    array.push("I love the Beatles!");
+    ++number;
+  } while(number < 15);
+  
+  return array;
 }
